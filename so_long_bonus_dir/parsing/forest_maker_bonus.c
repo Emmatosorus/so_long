d0b5ec9df465ba2d40c36fda4a018b44eb7b9202
Emmatosorus <emma@bouteyres.com>
2024-02-23 22:56:35 +0100

    Voided forest function

--- so_long_bonus_dir/parsing/forest_maker_bonus.c
+++ so_long_bonus_dir/parsing/forest_maker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: epolitze <epolitze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:42:25 by epolitze          #+#    #+#             */
-/*   Updated: 2024/02/19 17:19:43 by epolitze         ###   ########.fr       */
+/*   Updated: 2024/02/23 22:55:52 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	make_forest(t_main *main, int *s_pos, int *pos)
 {
-	if (s_pos[1] != 0 && main->map.s_map[s_pos[1] - 1][s_pos[0]])
+	(void)main;
+	(void)s_pos;
+	(void)pos;
+	//if (s_pos[1] != 0 && main->map.s_map[s_pos[1] - 1][s_pos[0]])
 }
